35ca68c37e2d5c32aee5068c51485298b73f7a10
Esteban Bernal <guillermo.bernal@mail.escuelaing.edu.co>
2020-10-21 14:57:52 -0500

    falta mapa y cache

--- src/main/resources/static/js/OpenWeatherApp.js
+++ src/main/resources/static/js/OpenWeatherApp.js
@@ -4,6 +4,31 @@
  * and open the template in the editor.
  */
 
+var cliente = OpenWeatherClient;
+var ciudad;
 
 var OpenWeatherApp = (function () {
+    function pintar(data) {
+        if (data == null) {
+            return new Error("No se encontro");
+        }
+        lista = [];
+        var lista = data.map(function (pb) {
+            return {weather: pb.weather.main, tempMax: pb.main.temp_max, tempMin: pb.main.temp_min, Cloud: pb.clouds.all}
+        })
+
+        lista.map(function (pb) {
+
+            var fila = <tr><td id="weather"> ${pb.weather} </td><td id="tempMax">${pb.tempMax}</td><td id="tempMin"> ${pb.tempMin} </td><td id="Cloud"> ${pb.Cloud}</td> </tr>;
+            $("#t01  tbody").append(fila);
+
+        })
+    }
+
+    return {
+        WeatherByNameCity: function (nombre) {
+            cliente.WeatherByNameCity(nombre, pintar);
+        },
+    };
+
 })();
